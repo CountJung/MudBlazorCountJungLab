596afeeaf875e572606cb6ea567098a6f4f66d0a
CountJung <ultimalife@naver.com>
2023-09-21 17:33:55 +0900

    image post and download test

--- MudBlazorCountJungLab/Models/InstarPostItem.cs
+++ MudBlazorCountJungLab/Models/InstarPostItem.cs
@@ -4,19 +4,23 @@ namespace MudBlazorCountJungLab.Models
 {
     public class InstarPostItem
     {
-        public InstarPostItem() 
+        public InstarPostItem()
         {
             Id = new();
         }
         public Guid Id { get; set; }
         [Required]
+        public string? Author { get; set; }
+        [Required]
+        [StringLength(10, ErrorMessage = "Title length less than 10")]
+        public string? Title { get; set; }
+        public ushort FavoriteCount { get; set; }
+        [Required]
         [StringLength(2000, MinimumLength = 3, ErrorMessage = "Max Character=2000, Min=3")]
         public string? Description { get; set; }
         public DateTime? RecordDateTime { get; set; }
         [Required]
-        [StringLength(1024*1024*10, ErrorMessage = "Image Size must lower than 10mb")]
+        [StringLength(1024 * 1024 * 10, ErrorMessage = "Image Size must lower than 10mb")]
         public string? Base64ImageData { get; set; }
-        [Required]
-        public string? Author { get; set; }
     }
 }
