a6e950565d8743b48b5f4e446397544ffb42347d
CountJung <ultimalife@naver.com>
2023-10-11 06:36:44 +0000

    Deploying to gh-pages from  @ 684db16595ec05210728f07c585a2c5a617f7619 🚀

---
+++ js/googleOAuth.js
@@ -0,0 +1,9 @@
+﻿let googleAuthenticationStateProviderInstance = null;
+export function GoogleAuthInitialize(clientId, googleAuthenticationStateProvider) {
+    googleAuthenticationStateProviderInstance = googleAuthenticationStateProvider;
+    google.accounts.id.initialize({ client_id: clientId, callback: blazorSchoolCallback });
+}
+
+function blazorSchoolCallback(googleResponse) {
+    googleAuthenticationStateProviderInstance.invokeMethodAsync("GoogleLogin", { ClientId: googleResponse.clientId, SelectedBy: googleResponse.select_by, Credential: googleResponse.credential });
+}
