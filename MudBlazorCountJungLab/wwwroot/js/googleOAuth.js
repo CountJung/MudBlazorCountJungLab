90602605014d9c18b8a70082739abfebf30f5956
CountJung <ultimalife@naver.com>
2023-11-03 10:57:49 +0900

    login button change

--- MudBlazorCountJungLab/wwwroot/js/googleOAuth.js
+++ MudBlazorCountJungLab/wwwroot/js/googleOAuth.js
@@ -4,6 +4,15 @@ export function GoogleAuthInitialize(clientId, googleAuthenticationStateProvider
     google.accounts.id.initialize({ client_id: clientId, callback: blazorSchoolCallback });
 }
 
+export function blazorSchoolGooglePrompt() {
+    google.accounts.id.prompt((notification) => {
+        if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
+            console.info(notification.getNotDisplayedReason());
+            console.info(notification.getSkippedReason());
+        }
+    });
+}
+
 function blazorSchoolCallback(googleResponse) {
     googleAuthenticationStateProviderInstance.invokeMethodAsync("GoogleLogin", { ClientId: googleResponse.clientId, SelectedBy: googleResponse.select_by, Credential: googleResponse.credential });
 }
